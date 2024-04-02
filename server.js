8e9c0608619cf0014d19297b55fad55545c0dcb6
Abinesh <101916855+abinesh-code@users.noreply.github.com>
2024-04-03 00:08:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const proxy = httpProxy.createProxyServer();
 // Proxy server
 const server = http.createServer((req, res) => {
     // Forward request to backend API
-    proxy.web(req, res, { target: 'http://3.106.227.95:8080' });
+    proxy.web(req, res, { target: 'http://13.54.7.53:8080' });
 });
 
 // Error handling for proxy
